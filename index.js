6fc8d4a3e124f925cc7c654101ba4164609f8336
Katie Carroll <ktcarroll4@gmail.com>
2020-10-09 21:37:00 -0500

    Appended json data to dom

--- index.js
+++ index.js
@@ -1,62 +1,10 @@
-// NASA API Info
-// apiKey="NhmXA7rpEvOYKSTQLjpGHNpnk7puUY54wiHuT2eO"
-// searchURL="https://api.nasa.gov/planetary/"
-
 
 $( ".icon-top" ).click(function() {
   $(".horoscope").toggleClass('hide');
-
-});
-
-const searchURL="https://json.astrologyapi.com/v1/sun_sign_prediction/daily/"
-
-function displayResults(responseJson) {
-  console.log(responseJson);
-  $(".horoscope").empty();
-  for (let i = 0; i < responseJson.items.length; i++) {
-  $(".icon-top").click(function(){
-    $(".horoscope").append(
-      `<li><h3>${responseJson.data[i].fullname}</h3>
-      <p>${responseJson.data[i].description}</p>
-      <p><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></p>
-      </li>`
-    );
-  }
-}
-
- function getDailyHoroscope(params)
-  const params = {
-    zodiacName: 'aries, taurus, gemini, cancer, leo, virgo, libra, scorpio, sagittarius, capricorn, aquarius, pisces'
-  };
-   const queryString = formatQueryParams(params);
-   const url = searchURL + "?" + queryString;
-
-   console.log(url);
-
-  fetch(url)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then((responseJson) => displayResults(responseJson))
-    .catch((err) => {
-      $("#js-error-message").text(`Something went wrong: ${err.message}`);
-    });
-}
-
-function watchForm() {
-  $("form").submit((event) => {
-    event.preventDefault();
-    const searchTerm = $(".icon-top").val();
-    const maxResults = $("#js-max-results").val();
-    getDailyHoroscope(searchTerm, maxResults);
+  fetch("http://ohmanda.com/api/horoscope/virgo")
+  .then(response => response.json())
+  .then(data => { $(".daily-horoscope").append(JSON.stringify(data))
+     
   });
-}
-
-$(watchForm);
-
-
-
+});
 
